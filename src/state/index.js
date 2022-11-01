9e72606d9f6450b605bad516c02c884517532cb6
argenych17 <kylychbekovargen14@gmail.com>
2022-11-01 20:00:08 +0600

    feat: configure redux toolkit

---
+++ src/state/index.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { todoSliceReducer } from "./todoSlice";
+
+const reducers  = combineReducers({
+  todos: todoSliceReducer
+})
+
+ export const store = configureStore({
+  reducer: reducers  
+  
+})
